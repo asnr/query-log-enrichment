c6937632f4b283e10b4f0c5aea06ac5826b73e0d
asnr <asnr@users.noreply.github.com>
2020-01-10 17:29:39 -0800

    Support shifting by timezone offset using FORMAT_TIMESTAMP

--- src/Enrich.hs
+++ src/Enrich.hs
@@ -9,9 +9,15 @@ import qualified Data.HashMap.Strict as HMS
 import Data.Proxy
 import Data.Text.Lazy.Encoding (decodeUtf8)
 import qualified Data.Text.Lazy as TL
-import Data.Time (NominalDiffTime, UTCTime(..), diffUTCTime)
+import Data.Time (NominalDiffTime,
+                  TimeZone(..),
+                  UTCTime(..),
+                  addUTCTime,
+                  diffUTCTime,
+                  secondsToDiffTime)
 import Data.Time.Calendar (Day, fromGregorian)
 import Data.Time.Format (defaultTimeLocale, parseTimeOrError)
+import Data.Time.Zones (loadSystemTZ, timeZoneForUTCTime)
 import Database.Sql.Util.Scope (runResolverWarn)
 import Database.Sql.Type (Query(..), Statement(..))
 import Database.Sql.Type.Query (Constant (..),
@@ -43,12 +49,67 @@ import Database.Sql.Vertica.Type (VerticaStatement (..),
                                   Vertica)
 
 enrich :: IO ()
-enrich = putStr . show  $ extractPartitions queryTime
-  "select _time from foo where _time > '2019-12-10'"
-  where queryTime = UTCTime (fromGregorian 2019 12 30) 0
+enrich =
+  let queryTime = UTCTime (fromGregorian 2019 12 30) 0
+      query = "select _time from foo where _time > '2019-12-10'"
+  in do partitions <- extractPartitions queryTime query
+        putStr $ show partitions
 
-extractPartitions :: UTCTime -> TL.Text -> [Partition]
-extractPartitions queryTime = getQueriedPartitions queryTime . fst . parseAndResolve
+data Partition = Partition
+  { partitionColumn :: FQCN
+  , earliestDate :: NominalDiffTime
+  } deriving Show
+
+data PartitionExpr = PartitionExpr
+  { partitionExprColumn :: FQCN
+  , earliestDateExpr :: Expr ResolvedNames ()
+  } deriving Show
+
+extractPartitions :: UTCTime -> TL.Text -> IO [Partition]
+extractPartitions queryTime =
+  mapM (evalPartitionExpr queryTime) .
+  getQueriedPartitions queryTime .
+  fst .
+  parseAndResolve
+
+evalPartitionExpr :: UTCTime -> PartitionExpr -> IO Partition
+evalPartitionExpr queryTime (PartitionExpr col expr) =
+  do timeDiff <- evalTimeDiff queryTime expr
+     return $ Partition col timeDiff
+
+evalTimeDiff :: UTCTime -> Expr ResolvedNames () -> IO NominalDiffTime
+evalTimeDiff qTime expr =
+  do time <- evalTimeExpr qTime expr
+     return $ diffUTCTime qTime time
+
+evalTimeExpr :: UTCTime -> Expr ResolvedNames () -> IO UTCTime
+evalTimeExpr qTime (ConstantExpr _ (StringConstant _ const)) =
+  return $
+    parseTimeOrError True defaultTimeLocale "%Y-%m-%d" $
+      TL.unpack $ decodeUtf8 const
+evalTimeExpr qTime (FunctionExpr _ funcName _ args _ _ _) =
+  case funcName of
+    (QFunctionName _ _ "current_timestamp") -> return qTime
+    (QFunctionName _ _ "timestamp") -> evalTimeExpr qTime $ head args
+    (QFunctionName _ _ "format_timestamp") ->
+      -- We will assume that we're considering an expression like
+      --
+      --   TIMESTAMP(
+      --     FORMAT_TIMESTAMP('%F %T', CURRENT_TIMESTAMP(), 'America/Los_Angeles')
+      --   )
+      --
+      -- which translates time (without timezone) by some offset. To be more
+      -- robust, we'd need to implement FORMAT_TIMESTAMP so that it returns a
+      -- string, not a time. Doing this would greatly complicate our
+      -- implementation, so we are avoiding it for now.
+      let timeExpr = args!!1
+          timezoneString = case args!!2 of
+            (ConstantExpr _ (StringConstant _ const)) -> TL.unpack $ decodeUtf8 const
+       in do time <- evalTimeExpr qTime timeExpr
+             tz <- loadSystemTZ timezoneString
+             let minutesOffset = timeZoneMinutes $ timeZoneForUTCTime tz time
+             let offset = 60 * (fromIntegral minutesOffset)
+             return $ addUTCTime offset time
 
 parseAndResolve :: TL.Text -> (VerticaStatement ResolvedNames (), [ResolutionError ()])
 parseAndResolve sql =
@@ -78,10 +139,8 @@ catalog = makeDefaultingCatalog catalogMap [defaultSchema] defaultDatabase
     catalogMap = HMS.singleton defaultDatabase $
                      HMS.fromList [ ( defaultSchema, HMS.fromList [ foo ] ) ]
 
-data Partition = Partition { partitionColumn :: FQCN, earliestDate :: NominalDiffTime }
-  deriving Show
 
-getQueriedPartitions :: UTCTime -> VerticaStatement ResolvedNames () -> [Partition]
+getQueriedPartitions :: UTCTime -> VerticaStatement ResolvedNames () -> [PartitionExpr]
 getQueriedPartitions queryTime (VerticaStandardSqlStatement s) = getPartitionsFromStatement queryTime s
 getQueriedPartitions _ (VerticaCreateProjectionStatement _) = []
 getQueriedPartitions _ (VerticaMultipleRenameStatement _) = []
@@ -89,7 +148,7 @@ getQueriedPartitions _ (VerticaSetSchemaStatement _) = []
 getQueriedPartitions _ (VerticaMergeStatement _) = []
 getQueriedPartitions _ (VerticaUnhandledStatement _) = []
 
-getPartitionsFromStatement :: UTCTime -> Statement Vertica ResolvedNames () -> [Partition]
+getPartitionsFromStatement :: UTCTime -> Statement Vertica ResolvedNames () -> [PartitionExpr]
 getPartitionsFromStatement qTime (QueryStmt q) = getPartitionsFromQuery qTime q
 getPartitionsFromStatement _ (InsertStmt _) = []
 getPartitionsFromStatement _ (UpdateStmt _) = []
@@ -109,7 +168,7 @@ getPartitionsFromStatement _ (RollbackStmt _) = []
 getPartitionsFromStatement _ (ExplainStmt _ _) = []
 getPartitionsFromStatement _ (EmptyStmt _) = []
 
-getPartitionsFromQuery :: UTCTime -> Query ResolvedNames () -> [Partition]
+getPartitionsFromQuery :: UTCTime -> Query ResolvedNames () -> [PartitionExpr]
 getPartitionsFromQuery qTime (QuerySelect _ s) = getPartitionsFromSelect qTime s
 getPartitionsFromQuery _ (QueryExcept _ _ _ _) = []
 getPartitionsFromQuery qTime (QueryUnion _ isDistinct _ fstQuery sndQuery) =
@@ -120,23 +179,23 @@ getPartitionsFromQuery qTime (QueryOrder _ _ q) = getPartitionsFromQuery qTime q
 getPartitionsFromQuery qTime (QueryLimit _ _ q) = getPartitionsFromQuery qTime q
 getPartitionsFromQuery qTime (QueryOffset _ _ q) = getPartitionsFromQuery qTime q
 
-getPartitionsFromSelect :: UTCTime -> Select ResolvedNames () -> [Partition]
+getPartitionsFromSelect :: UTCTime -> Select ResolvedNames () -> [PartitionExpr]
 getPartitionsFromSelect qTime select = case selectWhere select of
   Nothing -> []
   Just whereClause -> getPartitionsFromWhere qTime whereClause
 
-getPartitionsFromWhere :: UTCTime -> SelectWhere ResolvedNames () -> [Partition]
+getPartitionsFromWhere :: UTCTime -> SelectWhere ResolvedNames () -> [PartitionExpr]
 getPartitionsFromWhere qTime (SelectWhere _ expr) = getPartitionsFromExpr qTime expr
 
-getPartitionsFromExpr :: UTCTime -> Expr ResolvedNames () -> [Partition]
+getPartitionsFromExpr :: UTCTime -> Expr ResolvedNames () -> [PartitionExpr]
 getPartitionsFromExpr qTime (BinOpExpr _ (Operator ">") left right) =
   case [getPartitionExpr left, getPartitionExpr right] of
     [Nothing, Just fqcn] ->
-      [Partition fqcn (evalExpr qTime left)]
+      [PartitionExpr fqcn left]
     [Just fqcn, Nothing] ->
-      [Partition fqcn (evalExpr qTime right)]
+      [PartitionExpr fqcn right]
 
-unionPartitions :: [Partition] -> [Partition] -> [Partition]
+unionPartitions :: [PartitionExpr] -> [PartitionExpr] -> [PartitionExpr]
 unionPartitions a b = a
 
 getPartitionExpr :: Expr ResolvedNames () -> Maybe FQCN
@@ -147,10 +206,3 @@ getPartitionExpr expr = case expr of
     foldr (<|>) empty $ fmap getPartitionExpr args
   _ -> Nothing
 
-evalExpr :: UTCTime -> Expr ResolvedNames () -> NominalDiffTime
-evalExpr qTime (ConstantExpr _ (StringConstant _ const)) =
-  diffUTCTime qTime $
-    parseTimeOrError True defaultTimeLocale "%Y-%m-%d" $ TL.unpack $ decodeUtf8 const
-evalExpr qTime (FunctionExpr _ funcName _ _ _ _ _) =
-  case funcName of
-    (QFunctionName _ _ "current_timestamp") -> 0  -- qTime == current_timestamp()
--- test/Spec.hs
+++ test/Spec.hs
@@ -11,18 +11,28 @@ import Test.Tasty.HUnit (assertEqual, testCase)
 
 timeOfQuery = UTCTime (fromGregorian 2019 12 30) 0
 
+nominalHour = 60 * 60
+
 main :: IO ()
 main = defaultMain $ testGroup "Extract partitions"
   [
     testCase "Simple query" $
-      assertEqual "" nominalDay $ earliestDate $ head $
-        extractPartitions timeOfQuery "select _time from foo where _time > '2019-12-29'",
+      do let query = "select _time from foo where _time > '2019-12-29'"
+         partitions <- extractPartitions timeOfQuery query
+         assertEqual "" nominalDay $ earliestDate $ head partitions,
 
     testCase "Query comparing date" $
-      assertEqual "" (2 * nominalDay) $ earliestDate $ head $
-        extractPartitions timeOfQuery "select _time from foo where date(_time) > '2019-12-28'",
+      do let query = "select _time from foo where date(_time) > '2019-12-28'"
+         partitions <- extractPartitions timeOfQuery query
+         assertEqual "" (2 * nominalDay) $ earliestDate $ head partitions,
 
     testCase "Query comparing CURRENT_TIMESTAMP" $
-      assertEqual "" 0 $ earliestDate $ head $
-        extractPartitions timeOfQuery "select _time from foo where _time > CURRENT_TIMESTAMP(1)"
+      do let query = "select _time from foo where _time > CURRENT_TIMESTAMP(1)"
+         partitions <- extractPartitions timeOfQuery query
+         assertEqual "" 0 $ earliestDate $ head partitions,
+
+    testCase "Query comparing CURRENT_TIMESTAMP shifted by a timezone offset" $
+      do let query = "select _time from foo where _time > TIMESTAMP(FORMAT_TIMESTAMP('%F %T', CURRENT_TIMESTAMP(), 'America/Los_Angeles'))"
+         partitions <- extractPartitions timeOfQuery query
+         assertEqual "" (8 * nominalHour)  $ earliestDate $ head partitions
   ]
